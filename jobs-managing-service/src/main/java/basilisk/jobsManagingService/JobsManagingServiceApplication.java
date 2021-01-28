bf17354d4612f0f35b2b329abde9b486f1dd0b5b
Fakhr shaheen <fakhrshaheen@gmail.com>
2021-01-28 22:19:16 +0100

    initial add of jobs Managing service

---
+++ jobs-managing-service/src/main/java/basilisk/jobsManagingService/JobsManagingServiceApplication.java
@@ -0,0 +1,13 @@
+package basilisk.jobsManagingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsManagingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobsManagingServiceApplication.class, args);
+    }
+
+}

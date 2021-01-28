7726568745cefb77f1834f5e530721f7b34044ff
Fakhr shaheen <fakhrshaheen@gmail.com>
2021-01-28 22:24:27 +0100

    initial add of benchmark service

---
+++ benchmark-service/src/main/java/basilisk/benchmarkService/BenchmarkServiceApplication.java
@@ -0,0 +1,13 @@
+package basilisk.benchmarkService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchmarkServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BenchmarkServiceApplication.class, args);
+    }
+
+}

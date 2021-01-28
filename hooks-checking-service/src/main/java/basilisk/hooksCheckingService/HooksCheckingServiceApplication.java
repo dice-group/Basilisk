9d617c4bf612c78dc0354b0002d7028fd6cdd96d
Fakhr shaheen <fakhrshaheen@gmail.com>
2021-01-28 22:04:58 +0100

    initial add of hooks checking service

---
+++ hooks-checking-service/src/main/java/basilisk/hooksCheckingService/HooksCheckingServiceApplication.java
@@ -0,0 +1,13 @@
+package basilisk.hooksCheckingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooksCheckingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HooksCheckingServiceApplication.class, args);
+    }
+
+}
---
+++ hooks-checking-service/src/test/java/basilisk/hooksCheckingService/HooksCheckingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package basilisk.hooksCheckingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HooksCheckingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

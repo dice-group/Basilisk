648bd2c7821cd2bd30a2e193685ce01a12ff004c
Fakhr shaheen <fakhrshaheen@gmail.com>
2021-04-15 10:59:12 +0200

    add BenchmarkingService

---
+++ benchmark-service/src/main/java/basilisk/benchmarkService/services/BenchmarkingService.java
@@ -0,0 +1,11 @@
+package basilisk.benchmarkService.services;
+
+import basilisk.benchmarkService.domain.Iguana.IguanaConfiguration;
+
+/**
+ * @author Fakhr Shaheen
+ */
+public interface BenchmarkingService {
+
+    public void performBenchmark(IguanaConfiguration iguanaConfiguration);
+}

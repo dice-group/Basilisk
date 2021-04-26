98b75b5f4fed08e3509fd646a6405abbb78824d4
Fakhr shaheen <fakhrshaheen@gmail.com>
2021-04-26 18:35:14 +0200

    add TripleStoreRepository

---
+++ jobs-managing-service/src/main/java/basilisk/jobsManagingService/repositories/TripleStoreRepository.java
@@ -0,0 +1,17 @@
+package basilisk.jobsManagingService.repositories;
+
+import basilisk.jobsManagingService.domain.TripleStore;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * @author Fakhr Shaheen
+ */
+
+@Repository
+public interface TripleStoreRepository extends CrudRepository<TripleStore,Long> {
+
+    Optional<TripleStore> findTripleStoreByGitRepo_Id(Long gitRepoId);
+}

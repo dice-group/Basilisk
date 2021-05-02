cec8b8ca11c2d3b45327265355191c15ff1ca522
Fakhr shaheen <fakhrshaheen@gmail.com>
2021-05-02 16:54:57 +0200

    add actual attributes

--- jobs-managing-service/src/main/java/basilisk/jobsManagingService/events/BenchmarkJob/BenchmarkJobCreatedEvent.java
+++ jobs-managing-service/src/main/java/basilisk/jobsManagingService/events/BenchmarkJob/BenchmarkJobCreatedEvent.java
@@ -1,7 +1,17 @@
 package basilisk.jobsManagingService.events.BenchmarkJob;
 
+import basilisk.jobsManagingService.domain.benchmarking.BenchmarkJob;
+import lombok.Builder;
+
+import java.time.LocalDate;
+
 /**
  * @author Fakhr Shaheen
  */
+
+@Builder
 public class BenchmarkJobCreatedEvent {
+
+    private BenchmarkJob benchmarkJob;
+    private LocalDate createdDate;
 }

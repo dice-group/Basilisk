d65ef22489b77bc7032b16dd07111039a8bc41c3
Fakhr shaheen <fakhrshaheen@gmail.com>
2021-05-02 16:54:05 +0200

    add documentation

--- jobs-managing-service/src/main/java/basilisk/jobsManagingService/services/TripleStoreService.java
+++ jobs-managing-service/src/main/java/basilisk/jobsManagingService/services/TripleStoreService.java
@@ -10,12 +10,30 @@ import java.util.Optional;
  */
 public interface TripleStoreService {
 
+    /**
+     * add a triple store  to the system
+     * @param tripleStore
+     */
     void addTripleStore(TripleStore tripleStore);
 
+    /**
+     * retrieve a triple store
+     * @param id triple store's id
+     * @return
+     */
     Optional<TripleStore> getTripleStore(Long id);
 
+    /**
+     * retrieve a triple store
+     * @param id triple store's git repository id
+     * @return
+     */
     Optional<TripleStore> getTripleStoreByGitRepoId(Long id);
 
+    /**
+     * retrieve all triple stores
+     * @return
+     */
     List<TripleStore> getAllTripleStores();
 
 }

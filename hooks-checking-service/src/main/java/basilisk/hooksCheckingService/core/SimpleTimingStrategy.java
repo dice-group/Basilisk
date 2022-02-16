ad90ca08d8b948c1f401b5b1ac77f387f316b6b3
frensing <dev@frensing.de>
2022-02-16 15:40:12 +0100

    add timing timeout to properties

--- hooks-checking-service/src/main/java/basilisk/hooksCheckingService/core/SimpleTimingStrategy.java
+++ hooks-checking-service/src/main/java/basilisk/hooksCheckingService/core/SimpleTimingStrategy.java
@@ -1,17 +1,22 @@
 package basilisk.hooksCheckingService.core;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * @author Fakhr Shaheen
+ * @author Fakhr Shaheen, Fabian Rensing
  */
 
 @Component
-public class SimpleTimingStrategy implements TimingStrategy{
+public class SimpleTimingStrategy implements TimingStrategy {
+
+    @Value("${core.timing.timeout}")
+    private long timeout;
+
     @Override
     public void sleep() throws InterruptedException {
-        TimeUnit.SECONDS.sleep(5);
+        TimeUnit.SECONDS.sleep(timeout);
     }
 }

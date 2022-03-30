8c8f1684d3ff4b8fda1701ddbab54764fc8de5cf
frensing <dev@frensing.de>
2022-03-30 13:37:20 +0200

    pom update

--- jobs-managing-service/src/main/java/basilisk/jobsManagingService/web/messaging/benchmarking/BenchmarkMessageReceiver.java
+++ jobs-managing-service/src/main/java/basilisk/jobsManagingService/web/messaging/benchmarking/BenchmarkMessageReceiver.java
@@ -5,25 +5,18 @@ import basilisk.jobsManagingService.services.benchmarking.BenchmarkJobService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
-import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class RabbitMqBenchmarkMessageReceiver implements RabbitListenerConfigurer {
+public class BenchmarkMessageReceiver {
 
     private final BenchmarkJobService benchmarkJobService;
 
-    private final Logger logger = LoggerFactory.getLogger(RabbitMqBenchmarkMessageReceiver.class);
+    private final Logger logger = LoggerFactory.getLogger(BenchmarkMessageReceiver.class);
 
-    public RabbitMqBenchmarkMessageReceiver(BenchmarkJobService benchmarkJobService) {
+    public BenchmarkMessageReceiver(BenchmarkJobService benchmarkJobService) {
         this.benchmarkJobService = benchmarkJobService;
-    }
-
-    @Override
-    public void configureRabbitListeners(RabbitListenerEndpointRegistrar rabbitListenerEndpointRegistrar) {
-
     }
 
     @RabbitListener(queues = "${rabbitmq.benchmarks.jobQueue}")

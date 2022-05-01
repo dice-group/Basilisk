82a2d7654a95986671d73f8aec9bd581c075f983
frensing <dev@frensing.de>
2022-05-01 17:06:34 +0200

    add manual job creation

--- hooks-checking-service/src/main/java/org/dicegroup/basilisk/hooksCheckingService/HooksCheckingServiceApplication.java
+++ hooks-checking-service/src/main/java/org/dicegroup/basilisk/hooksCheckingService/HooksCheckingServiceApplication.java
@@ -10,8 +10,6 @@ public class HooksCheckingServiceApplication {
 
     public static void main(String[] args)  {
         SpringApplication.run(HooksCheckingServiceApplication.class, args);
-
-
     }
 
 }
